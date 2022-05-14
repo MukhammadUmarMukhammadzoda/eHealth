0188905e41f561ec86affd78b8c280b414097e67
Navideveloper <pythondarslar@gmail.com>
2022-05-14 12:12:22 +0300

    n5

--- API/models.py
+++ API/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from rest_framework.authtoken.models import Token
 # Create your models here.
 
 class TaskSport(models.Model):
--- eHealth/settings.py
+++ eHealth/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "rest_framework",
     "API",
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
